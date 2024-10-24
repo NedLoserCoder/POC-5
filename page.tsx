7f5a20785062a137ab6b7da5abd0d5df10878652
caiotakehiro <144562821+caiotakehiro@users.noreply.github.com>
2024-10-24 19:13:24 -0300

    Create page.tsx

---
+++ page.tsx
@@ -0,0 +1,12 @@
+import Header from './components/Header';
+
+export default function Home() {
+  return (
+    <div>
+      <Header />
+      <main>
+        <p>Esta é a página principal da aplicação Next.js.</p>
+      </main>
+    </div>
+  );
+}
